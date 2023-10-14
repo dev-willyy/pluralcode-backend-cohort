2c1b2ba0aafc274158369f483ae96dd00b4cc6fa
Williams Madu <williams.madu97@gmail.com>
2023-10-14 20:56:39 +0100

    Implemented passport.js - (outside of e-commerce project)

--- passport-auth/private/passport/app.js
+++ passport-auth/private/passport/app.js
@@ -16,8 +16,6 @@ const secret = '759fb4bb-48b1-4efe-845a-244b65999265';
  * Session is persisted on the DB, Cookie is persisted on the client
  */
 
-const users = [{ id: 1, username: 'Jidesky' }];
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -32,22 +30,33 @@ app.use(
   })
 );
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(
+  'login',
   new LocalStrategy((username, password, done) => {
     try {
-      if (username === 'fail' || password === 'password') return done(null, false);
+      if (username === 'fail' || password === 'password')
+        return done(null, false, { message: 'Invalid login credentials' });
       if (username === 'Mongodb error') throw new Error('Error interacting with MongoDB');
-      return done(null, { username, password });
+      return done(null, { username, password }, { message: 'login successful' });
     } catch (err) {
       console.error(err);
     }
   })
 );
 
+passport.serializeUser((user, done) => {
+  return done(null, user, console.log('Serializing user into session: ', user));
+});
+
+passport.deserializeUser((user, done) => {
+  return done(null, user, console.log('Deserializing user from session'));
+});
+
 app.get('/', (req, res, next) => {
-  req.session.user = users[0];
-  console.log(req.session);
+  console.log(req.session, req.user, req.isAuthenticated());
   res.status(200).send('Home route is functional');
 });
 
@@ -55,16 +64,30 @@ app.get('/login', (req, res, next) => {
   res.render('login');
 });
 
+/**
+ * fix req.session.messages === undefined for '/success'
+ * if (user.isLoggedIn() or isAuthenticated()) disallow access to '/register'
+ */
 app.get('/success', (req, res, next) => {
+  // console.log(req.user, req.login, req.logout, req.isAuthenticated, req.isPaused);
+  console.log(req.query, req.isAuthenticated());
   res.status(200).send('login successful');
 });
 
 app.get('/failure', (req, res, next) => {
+  console.log(req.session);
   res.status(401).send('login failed');
 });
 
 app.get('/login', (req, res, next) => {
   res.render('login');
+});
+
+app.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+  });
+  res.redirect('/');
 });
 
 app.post(
@@ -73,12 +96,18 @@ app.post(
     console.log('Processing user Sign in');
     next();
   },
-  passport.authenticate('local', {
-    session: false,
-    successRedirect: '/success',
+  passport.authenticate('login', {
+    successRedirect: '/success?message=success!%20login%20successful',
+    successMessage: true,
     failureRedirect: '/failure',
+    failureMessage: true,
   })
 );
+
+app.get('/top-secret', (req, res, next) => {
+  if (!req.isAuthenticated()) return res.redirect('/');
+  return res.status(200).send('Jesus, the Messiah is coming soon!');
+});
 
 const port = 3003;
 app.listen(port, () => {
